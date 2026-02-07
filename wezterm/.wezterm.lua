709309f455d86b799335ad9687e160cc80da5cd3
Joe Battistello <joebattistello@hotmail.com>
2026-02-07 02:22:24 -0500

    Update theming + add WezTerm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,33 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 10
+
+-- Noctalia theme (see ~/.config/wezterm/colors/noctalia.toml)
+config.color_scheme_dirs = { wezterm.home_dir .. '/.config/wezterm/colors' }
+config.color_scheme = 'noctalia'
+
+-- 90% opacity
+config.window_background_opacity = 0.90
+
+-- Tab bar cleanup
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = true
+config.tab_bar_at_bottom = false
+
+-- Force XWayland (Wayland backend is flaky in this setup)
+config.enable_wayland = false
+
+-- Finally, return the configuration to wezterm:
+return config
