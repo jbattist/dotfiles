881b77544c32594e5b977e220296114a02b15583
Joe Battistello <joe@joe.com>
2026-07-27 07:28:35 -0400

    new file:   thefuck/.config/thefuck/settings.py

---
+++ thefuck/.config/thefuck/settings.py
@@ -0,0 +1,37 @@
+# The Fuck settings file
+#
+# The rules are defined as in the example bellow:
+#
+# rules = ['cd_parent', 'git_push', 'python_command', 'sudo']
+#
+# The default values are as follows. Uncomment and change to fit your needs.
+# See https://github.com/nvbn/thefuck#settings for more information.
+#
+
+# rules = [<const: All rules enabled>]
+# exclude_rules = []
+# wait_command = 3
+# require_confirmation = True
+# no_colors = False
+# debug = False
+# priority = {}
+# history_limit = None
+# alter_history = True
+# wait_slow_command = 15
+# slow_commands = ['lein', 'react-native', 'gradle', './gradlew', 'vagrant']
+# repeat = False
+# instant_mode = False
+# num_close_matches = 3
+# env = {'LC_ALL': 'C', 'LANG': 'C', 'GIT_TRACE': '1'}
+# excluded_search_path_prefixes = []
+
+# Do not enable dangerous or workflow-opinionated corrections.
+exclude_rules = [
+    "git_branch_delete",
+    "git_hook_bypass",
+    "git_merge_unrelated",
+    "git_rm_local_modifications",
+    "git_rm_staged",
+    "git_tag_force",
+    "rm_dir",
+]
